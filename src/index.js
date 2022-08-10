905436dcc994b42b7551141554b9f659635ccdef
Sergey <sergey.grinkevich@gmail.com>
2022-08-11 01:17:49 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
   
+    let rever = 0;
+    
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+    
+
+    while(n > 0) {
+      rever = rever * 10 + n % 10;
+      n = Math.floor(n / 10);
+    }
+     return rever;
+  
+
+
 }
+
